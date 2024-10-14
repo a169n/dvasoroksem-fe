a5c57ed02d7d95b19f4af749dfd8508b2cbb1adc
Aibyn Talgatov <a1byn.talgat05@gmail.com>
2024-10-14 14:53:29 +0500

    new section

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,11 +1,16 @@
 import { HomeHeader } from "./ui/homeHeader";
 import { Services } from "./ui/serviceAccordions";
+import { WhyChooseUs } from "./ui/whyChooseUs";
 
 export const Home = () => {
   return (
     <div>
       <HomeHeader />
       <Services />
+      <div
+        style={{ backgroundColor: "#232323", width: "100%", height: "5%" }}
+      ></div>
+      <WhyChooseUs />
     </div>
   );
 };
