176ebaec848f76a76247e035cef7526d9775205a
Aibyn Talgatov <a1byn.talgat05@gmail.com>
2024-11-09 01:24:55 +0500

    feat: tailwind css

--- src/layout/DefaultLayout.tsx
+++ src/layout/DefaultLayout.tsx
@@ -1,17 +1,13 @@
-import { Box } from "@mui/material";
+import { Container } from "@mui/material";
 import { Footer } from "@shared/ui/footer";
-import { Header } from "@shared/ui/header";
 import { Outlet } from "react-router-dom";
 
 export function DefaultLayout() {
   return (
-    <div style={{ maxWidth: "2000px" }}>
-      <Header />
-      <Box sx={{ width: "100%", display: "flex", justifyContent: "center" }}>
-        <Outlet />
-      </Box>
+    <Container maxWidth={false} disableGutters sx={{ maxWidth: "1920px" }}>
+      <Outlet />
       <Footer />
-    </div>
+    </Container>
   );
 }
 
