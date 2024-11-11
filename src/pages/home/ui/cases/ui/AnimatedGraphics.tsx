cf599d359c70fbb1aa36ee8e80d9d718851ab245
Aibyn Talgatov <a1byn.talgat05@gmail.com>
2024-11-12 02:01:57 +0500

    fix

--- src/pages/home/ui/cases/ui/AnimatedGraphics.tsx
+++ src/pages/home/ui/cases/ui/AnimatedGraphics.tsx
@@ -27,14 +27,14 @@ const AnimatedGraphics = () => {
           strokeDashoffset: [anime.setDashoffset, 0],
           easing: "easeInOutSine",
           duration: 1000,
-          delay: (el, i) => i * 250,
+          delay: (i) => i * 250,
         })
         .add({
           targets: ".arrow-path",
           strokeDashoffset: [anime.setDashoffset, 0],
           easing: "easeInOutSine",
           duration: 2000,
-          delay: (el, i) => i * 200,
+          delay: (i) => i * 200,
         })
         .add({
           targets: ".swirl-path, .arrow-path",
@@ -52,7 +52,7 @@ const AnimatedGraphics = () => {
           strokeDashoffset: [0, anime.setDashoffset], // Reverse the stroke
           easing: "easeInOutSine",
           duration: 1000,
-          delay: (el, i) => i * 250, // Restart the animation
+          delay: (i) => i * 250, // Restart the animation
         });
     };
 
