7056cabc4b47e57f1bc9ff33f23589778aa52717
Aibyn Talgatov <a1byn.talgat05@gmail.com>
2024-11-30 01:45:52 +0500

    vide carousel

--- src/pages/cases/ui/coffee-boom/ui/coffeeBoomVideos/index.ts
+++
@@ -1,1 +0,0 @@
-export { CoffeeBoomVideos } from "./CoffeeBoomVideos";
--- src/pages/cases/ui/everest/Everest.tsx
+++ src/pages/cases/ui/everest/Everest.tsx
@@ -1,9 +1,16 @@
+import { useEffect } from "react";
 import { Box } from "@mui/material";
 import { Header } from "@shared/ui/header";
+import { LazyLoadImage } from "react-lazy-load-image-component";
 import EverestMainImage from "@assets/casePages/everest.svg";
 import { Description } from "@shared/ui/description";
 import { Cases } from "@src/pages/home/ui/cases";
+import DoubleCarousel from "@shared/ui/doubleCarousel/DoubleCarousel";
+import { Stories } from "@shared/ui/stories";
+import EverestData from "./ui/everestData";
+import "react-lazy-load-image-component/src/effects/blur.css";
 
+// Import images
 import everest1 from "@assets/casePages/everest/everest1.png";
 import everest2 from "@assets/casePages/everest/everest2.png";
 import everest3 from "@assets/casePages/everest/everest3.png";
@@ -18,27 +25,49 @@ import everestUnder4 from "@assets/casePages/everest/everestUnder4.png";
 import everestUnder5 from "@assets/casePages/everest/everestUnder5.png";
 import everestUnder6 from "@assets/casePages/everest/everestUnder6.png";
 import everestUnder7 from "@assets/casePages/everest/everestUnder7.png";
-import DoubleCarousel from "@shared/ui/doubleCarousel/DoubleCarousel";
 import everestStory1 from "@assets/casePages/everest/everestStory1.png";
 import everestStory2 from "@assets/casePages/everest/everestStory2.png";
 import everestStory3 from "@assets/casePages/everest/everestStory3.png";
-import { Stories } from "@shared/ui/stories";
-import EverestData from "./ui/everestData";
+
+// Import videos
+import everestReels1 from "@assets/videos/everest/everest_reels_1.mov";
+import everestReels2 from "@assets/videos/everest/everest_reels_2.mov";
+import everestReels3 from "@assets/videos/everest/everest_reels_3.mp4";
+import everestStories1 from "@assets/videos/everest/everest_stories_1.mov";
+import everestStories2 from "@assets/videos/everest/everest_stories_2.mov";
+import everestStories3 from "@assets/videos/everest/everest_stories_3.mov";
+import VideoCarousel from "@shared/ui/videoCarousel/VideoCarousel";
+
 export const Everest = () => {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
+  const videos = [
+    everestReels1,
+    everestReels2,
+    everestReels3,
+    everestStories1,
+    everestStories2,
+    everestStories3,
+  ];
+
   return (
     <>
-      <Header mode="dark"  />
-      <Box sx={{ minHeight: { xs: "200px", md: "none" } }}>
-        <img
+      <Header mode="dark" />
+      <Box sx={{ minHeight: { xs: "200px", md: "600px" } }}>
+        <LazyLoadImage
           src={EverestMainImage}
           alt="everest main image"
-          style={{ width: "100%", height: "auto" }}
+          width="100%"
+          effect="blur"
         />
       </Box>
       <Description
         title="выше с нами"
         description="Ведем Instagram Everest’а — профессиональный волейбольный клуб в Астане. Наш подход к контенту нацелен на подростков: создаём посты с аниме, челленджами и тренерами, вовлекая молодёжь в спорт и волейбол. Помогаем клубу вдохновлять новое поколение спортсменов."
       />
+      <VideoCarousel videos={videos} />
       <DoubleCarousel
         imagesLine1={[
           everest1,
