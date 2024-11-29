c02b9fa84b340e9cc351ac1bbc8a2f378e93b46f
Aibyn Talgatov <a1byn.talgat05@gmail.com>
2024-11-30 00:54:55 +0500

    coffee boom videos

--- src/pages/cases/ui/coffee-boom/CoffeeBoom.tsx
+++ src/pages/cases/ui/coffee-boom/CoffeeBoom.tsx
@@ -1,31 +1,38 @@
+import { useEffect } from "react";
 import { Box } from "@mui/material";
 import { Header } from "@shared/ui/header";
+import { LazyLoadImage } from "react-lazy-load-image-component";
 import CoffeeBoomMainImage from "@assets/casePages/coffee-boom.svg";
 import { Description } from "@shared/ui/description";
 import { Cases } from "@pages/home/ui/cases";
 import CoffeeBoomData from "./ui/coffee-boomData";
-import { useEffect } from "react";
+import { CoffeeBoomVideos } from "./ui/coffeeBoomVideos";
+import "react-lazy-load-image-component/src/effects/blur.css";
+
 export const CoffeeBoom = () => {
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
+
   return (
-    <>
+    <Box sx={{ backgroundColor: "#161616" }}>
       <Header mode="light" />
-      <Box sx={{ minHeight: { xs: "200px", md: "none" } }}>
-        <img
+      <Box sx={{ minHeight: { xs: "200px", md: "600px" } }}>
+        <LazyLoadImage
           src={CoffeeBoomMainImage}
           alt="coffee boom main image"
-          style={{ width: "100%" }}
+          width="100%"
+          effect="blur"
         />
       </Box>
       <Description
-        title="место, где
-становятся друзьями"
-        description="Мы запустили TikTok для сети кофеен Coffee BOOM. Благодаря продуманной и актуальной контент-стратегии с нуля набрали 50 тысяч подписчиков."
+        title="место, где становятся друзьями"
+        description="Мы запустили TikTok для сети кофеен Coffee BOOM. Благодаря продуманной и актуальной
+        контент-стратегии с нуля набрали 50 тысяч подписчиков."
       />
       <CoffeeBoomData />
+      <CoffeeBoomVideos />
       <Cases mode="case-page" />
-    </>
+    </Box>
   );
 };
