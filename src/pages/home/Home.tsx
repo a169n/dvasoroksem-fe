6734c8701a5988ccf982cb38e0bf3d865f7252eb
Aibyn Talgatov <a1byn.talgat05@gmail.com>
2025-01-07 18:41:19 +0500

    home header optimization

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,27 +1,30 @@
-
-import { Header } from '@shared/ui/header';
-import { HomeHeader } from './ui/homeHeader';
-import { Services } from './ui/serviceAccordions';
-import { WhyChooseUs } from './ui/whyChooseUs';
-import { Cases } from './ui/cases';
-import { Apply } from './ui/cases/ui';
-import { Videos } from './ui/videos';
-import { Certificates } from './ui/certificatesSection';
-import { Values } from './ui/values';
-import { useLayoutContext } from '../../context/LayoutContext'; 
-import { useEffect } from 'react';
+import { Header } from "@shared/ui/header";
+import { Services } from "./ui/serviceAccordions";
+import { WhyChooseUs } from "./ui/whyChooseUs";
+import { Cases } from "./ui/cases";
+import { Apply } from "./ui/cases/ui";
+import { Videos } from "./ui/videos";
+import { Certificates } from "./ui/certificatesSection";
+import { Values } from "./ui/values";
+import { useLayoutContext } from "../../context/LayoutContext";
+import { useEffect } from "react";
+import HomeHeader from "./ui/homeHeader/HomeHeader";
 
 export const Home = () => {
   const { refs } = useLayoutContext();
   useEffect(() => {
     const state = window.history.state;
-    if (state && state.scrollToSection && refs[state.scrollToSection]?.current) {
+    if (
+      state &&
+      state.scrollToSection &&
+      refs[state.scrollToSection]?.current
+    ) {
       setTimeout(() => {
         const sectionRef = refs[state.scrollToSection];
         if (sectionRef?.current) {
           window.scrollTo({
             top: sectionRef.current.offsetTop,
-            behavior: 'smooth',
+            behavior: "smooth",
           });
         }
         window.history.replaceState({}, document.title);
